9950e5d48316fea166df1acf0554fd8fde712488
HudsonDukette <hudsoduk@gmail.com>
2026-04-16 18:00:43 +0000

    dddddfds

--- ant-war/render.js
+++ ant-war/render.js
@@ -1,8 +1,58 @@
+function drawAnt(a, color) {
+  let p = worldToScreen(a.x, a.y);
+
+  ctx.save();
+  ctx.translate(p.x, p.y);
+  ctx.rotate(a.angle || 0);
+
+  ctx.fillStyle = color;
+
+  ctx.fillRect(-6, -2, 4, 4);
+  ctx.fillRect(-2, -3, 5, 5);
+  ctx.fillRect(4, -2, 4, 4);
+
+  ctx.restore();
+}
+
+/* ✅ THIS WAS MISSING AND CAUSED YOUR CRASH */
+function drawWorld() {
+  let x = -camera.x % dirt.width;
+  let y = -camera.y % dirt.height;
+
+  for (let i = -1; i < 3; i++) {
+    for (let j = -1; j < 3; j++) {
+      ctx.drawImage(dirt, x + i * dirt.width, y + j * dirt.height);
+    }
+  }
+}
+
+function drawUI() {
+
+  if (gameState.mode === "menu") {
+    ctx.fillStyle = "black";
+    ctx.globalAlpha = 0.7;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 1;
+
+    ctx.fillStyle = "white";
+    ctx.font = "40px Arial";
+    ctx.fillText("ANT WAR", canvas.width / 2 - 100, canvas.height / 2);
+
+    ctx.font = "20px Arial";
+    ctx.fillText("Click to Start", canvas.width / 2 - 70, canvas.height / 2 + 40);
+    return;
+  }
+
+  ctx.fillStyle = "white";
+  ctx.font = "14px Arial";
+  ctx.fillText("Food: " + playerFood, 10, 20);
+  ctx.fillText("Ants: " + ants.length, 10, 40);
+  ctx.fillText("Colonies: " + enemyColonies.length, 10, 60);
+}
+
 function draw() {
+  drawWorld(); // now exists again → no crash
 
-  drawWorld();
-
-  // FOOD
   for (let f of food) {
     let p = worldToScreen(f.x, f.y);
     ctx.fillStyle = "lime";
@@ -11,12 +61,10 @@ function draw() {
     ctx.fill();
   }
 
-  // SURFACE ANTS
   for (let a of ants) {
     drawAnt(a, "white");
   }
 
-  // ENEMIES
   for (let c of enemyColonies) {
     let q = c.queen;
     let p = worldToScreen(q.x, q.y);
@@ -25,29 +73,25 @@ function draw() {
     ctx.beginPath();
     ctx.arc(p.x, p.y, 8, 0, Math.PI * 2);
     ctx.fill();
+
+    for (let w of c.workers) drawAnt(w, "orange");
+    for (let s of c.soldiers) drawAnt(s, "red");
   }
 
-  // COLONY SYSTEM (BURROW)
-  if (colony.burrowed) {
+  drawAnt(queen, "cyan");
 
+  if (colony?.burrowed) {
     let p = worldToScreen(colony.entrance.x, colony.entrance.y);
 
-    // tunnel entrance
     ctx.fillStyle = "black";
     ctx.beginPath();
     ctx.arc(p.x, p.y, 10, 0, Math.PI * 2);
     ctx.fill();
 
-    // defenders
     for (let d of colony.defenders) {
-      let dp = worldToScreen(d.x, d.y);
-      ctx.fillStyle = "blue";
-      ctx.beginPath();
-      ctx.arc(dp.x, dp.y, 5, 0, Math.PI * 2);
-      ctx.fill();
+      drawAnt(d, "blue");
     }
 
-    // eggs
     for (let e of colony.eggs) {
       let ep = worldToScreen(e.x, e.y);
       ctx.fillStyle = "yellow";
@@ -56,8 +100,6 @@ function draw() {
       ctx.fill();
     }
   }
-
-  drawAnt(queen, "cyan");
 
   drawUI();
 }
