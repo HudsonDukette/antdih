c2e84108ce35e7d62e7805dc18ce4f5c0e4bc651
HudsonDukette <hudsoduk@gmail.com>
2026-04-16 18:02:24 +0000

    dsdsdsdsdsasddf

--- ant-war/ants.js
+++ ant-war/ants.js
@@ -10,6 +10,99 @@ function makeAnt(type, x, y) {
   };
 }
 
+let ants = [];
+
 function spawnAnt(type) {
   ants.push(makeAnt(type, queen.x, queen.y));
+}
+
+function findClosestFood(x, y) {
+  let best = null;
+  let bestD = Infinity;
+
+  for (let f of food) {
+    let d = Math.hypot(f.x - x, f.y - y);
+    if (d < bestD) {
+      bestD = d;
+      best = f;
+    }
+  }
+
+  return best;
+}
+
+/* ✅ THIS IS THE FUNCTION YOU ARE MISSING */
+function updateAnts() {
+
+  for (let a of ants) {
+
+    // SOLDIERS
+    if (a.type === "soldier") {
+
+      let target = findNearestEnemy(a.x, a.y, "player");
+
+      if (target && target.ref) {
+
+        let dx = target.x - a.x;
+        let dy = target.y - a.y;
+        let d = Math.hypot(dx, dy);
+
+        a.angle = Math.atan2(dy, dx);
+
+        if (d < 10) {
+          target.ref.hp -= 1;
+
+          if (target.ref.hp <= 0) {
+            target.ref.dead = true;
+          }
+
+        } else {
+          a.x += Math.cos(a.angle) * a.speed;
+          a.y += Math.sin(a.angle) * a.speed;
+        }
+      }
+
+      continue;
+    }
+
+    // WORKERS
+    if (!a.carrying) {
+
+      let t = findClosestFood(a.x, a.y);
+      if (!t) continue;
+
+      let dx = t.x - a.x;
+      let dy = t.y - a.y;
+      let d = Math.hypot(dx, dy);
+
+      a.angle = Math.atan2(dy, dx);
+
+      if (d < 6) {
+        let i = food.indexOf(t);
+        if (i !== -1) food.splice(i, 1);
+        a.carrying = true;
+      } else {
+        a.x += Math.cos(a.angle) * a.speed;
+        a.y += Math.sin(a.angle) * a.speed;
+      }
+
+    } else {
+
+      let dx = queen.x - a.x;
+      let dy = queen.y - a.y;
+      let d = Math.hypot(dx, dy);
+
+      a.angle = Math.atan2(dy, dx);
+
+      if (d < 10) {
+        a.carrying = false;
+        playerFood++;
+      } else {
+        a.x += Math.cos(a.angle) * a.speed;
+        a.y += Math.sin(a.angle) * a.speed;
+      }
+    }
+  }
+
+  ants = ants.filter(a => a.hp > 0);
 }
